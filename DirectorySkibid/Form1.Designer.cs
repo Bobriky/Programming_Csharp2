5bd2a042afe953239c4e5de8c3f899748da076ae
Šimon Frőmel <fromelsimon2@gmail.com>
2025-01-28 13:53:43 +0100

    hg

---
+++ DirectorySkibid/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace DirectorySkibid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblDirectory = new System.Windows.Forms.ToolStripStatusLabel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fldD = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblDirectory});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(705, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblDirectory
+            // 
+            this.lblDirectory.Name = "lblDirectory";
+            this.lblDirectory.Size = new System.Drawing.Size(118, 17);
+            this.lblDirectory.Text = "toolStripStatusLabel1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(49, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(519, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(49, 97);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(519, 290);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "SELECT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblDirectory;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog fldD;
+    }
+}
+
---
+++ DirectorySkibid/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DirectorySkibid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<string> seznam = new List<string>();
+            seznam.Add("urur");
+            seznam.Add("ponke");
+            seznam.Add("brett");
+            seznam.Add("KrasbergIssue");
+            Directory.SetCurrentDirectory("C:\\");     // skibidi C:\\ pro nalezení kořenu skibida
+            lblDirectory.Text = Directory.GetCurrentDirectory();
+            Directory.CreateDirectory(@"C:\myDir");         //nebo alternativa   @"C:\
+            foreach(string item in seznam)
+            {
+                Directory.CreateDirectory("C:\\myDir"+ item);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(fldD.ShowDialog() == DialogResult.OK)
+            {/*
+                foreach (itenm in (Directory.EnumerateFileSystemEntries(Directory.GetCurrentDirectory()))
+                {
+
+                }
+                Directory.SetCurrentDirectory(fldD.SelectedPath);
+                lblDirectory.Text = Directory.GetCurrentDirectory();
+                listBox1.Items.Add(Directory.GetCurrentDirectory().ToString());
+                comboBox1.Items.Add(Directory.EnumerateFileSystemEntries(Directory.GetCurrentDirectory()));*/
+            }
+        }
+    }
+}
