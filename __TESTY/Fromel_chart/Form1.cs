8be341696e8285315b4f44b5059867f8ac5c6499
Šimon Frőmel <fromelsimon2@gmail.com>
2025-01-28 14:06:53 +0100

    gdgfd

---
+++ __TESTY/Fromel_chart/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Fromel_chart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {       //
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader file = new StreamReader(openFileDialog.FileName, Encoding.Default);
+                    //string[] polozky;
+                    file.ReadLine();
+                    file.ReadLine();
+                    while (!file.EndOfStream)
+                    {
+                        string[] polozky = file.ReadLine().Split(';');
+                        ListViewItem row = new ListViewItem(polozky[0]);
+                        for(int i = 1; i < polozky.Length; i++)
+                        {
+                            row.SubItems.Add(polozky[i]);
+                        }
+                        lstData.Items.Add(row);
+                    }
+                    file.Close();
+
+                    for (int a = 0; a < lstData.Items.Count; a++)
+                    {
+                        chart.Series[0].Points.AddXY(Convert.ToDouble(lstData.Items[a].SubItems[1]), Convert.ToDouble(lstData.Items[a].SubItems[2]));
+                    }
+                }
+            }
+            catch (Exception)
+            {
+               MessageBox.Show("Graf nefunguje");
+            }
+        }
+    }
+}
---
+++ zk04charSin/chartSin/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chartSin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cmbFunkce.SelectedIndex = 0;
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            double amplitude;
+                for (double i = 0; i <= 360; i++)
+            {
+                 amplitude = (double)nmAmpl.Value * (double)Math.Sin(2 * Math.PI * (double)nmFreq.Value * i);
+                chart1.Series[0].Points.AddXY(i, amplitude);
+            }
+        }
+
+        private void nmAmpl_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
